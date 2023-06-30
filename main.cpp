f1bdb9186b40a810a9b244f4d654ea7157a5bb5a
madyisus <gonzalez.29la@gmail.com>
2023-06-29 23:52:56 -0400

    Esqueleto base para testing

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include "distance.cpp"
+#include "getBook.cpp"
 #include <iostream>
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 
-string source;
-string target;
+if (argc != 3) {
+    cout << "Se requieren dos paths como argumentos." << endl;
+    return 1;
+}
 
-source = "mamifero";
-target = "mamal";
+string path1 = argv[1];     // path al primer input
+string path2 = argv[2];     // path al segundo input
 
-Solution2 solClasica;
-int distance = solClasica.minDistance(source,target);
+string book1 = readFileContent(path1);      // libro 1 completo como string
+string book2 = readFileContent(path2);      // libro 2 completo como string
 
-cout<<"Distance between "<<source<<" and "<<target<<": "<<distance<<endl;
+cout<<"Tamaño del libro 1:  "<<book1.size()<<endl;
+cout<<"Tamaño del libro 2:  "<<book2.size()<<endl;
+
+Solution2 solClasica;       // instancia del tipo de solucion clasica
+Solution3 solAdaptativa;    // instancia del tipo de solucion adaptativa
+
+int d_classic = solClasica.minDistance(book1,book2);        // calculo de la distancia usando metodo clasico
+int d_adaptive = solAdaptativa.minDistance(book1,book2);        // calculo de la distnacia usando metodo adaptativo
+
+cout<<"Distancia calculada con metodo clásico:      "<<d_classic<<endl;
+cout<<"Distancia calculada con metodo adaptativo:   "<<d_adaptive<<endl;
+
 return 0;
 }
