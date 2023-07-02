26826c744f477229342e8935ee3ce2e2a3d0c7d9
madyisus <gonzalez.29la@gmail.com>
2023-07-02 03:48:41 -0400

    version final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "distance.cpp"
-#include "getBook.cpp"
+#include "stuff.cpp"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -17,17 +18,20 @@ string path2 = argv[2];     // path al segundo input
 string book1 = readFileContent(path1);      // libro 1 completo como string
 string book2 = readFileContent(path2);      // libro 2 completo como string
 
-cout<<"Tamaño del libro 1:  "<<book1.size()<<endl;
-cout<<"Tamaño del libro 2:  "<<book2.size()<<endl;
+book1 = book1.substr(0,1200);
+book2 = book2.substr(0,1);
+
+// cout<<"Tamaño del libro 1:  "<<book1.length()<<" --> "<<sizeof(string) + book1.length()<<" bytes en memoria."<<endl;
+// cout<<"Tamaño del libro 2:  "<<book2.length()<<" --> "<<sizeof(string) + book2.length()<<" bytes en memoria."<<endl;
 
 Solution2 solClasica;       // instancia del tipo de solucion clasica
 Solution3 solAdaptativa;    // instancia del tipo de solucion adaptativa
 
-int d_classic = solClasica.minDistance(book1,book2);        // calculo de la distancia usando metodo clasico
-int d_adaptive = solAdaptativa.minDistance(book1,book2);        // calculo de la distnacia usando metodo adaptativo
+auto time1 = execTimeForKAttempts(solClasica,book1,book2,20);
+auto time2 = execTimeForKAttempts(solAdaptativa,book1,book2,20);
 
-cout<<"Distancia calculada con metodo clásico:      "<<d_classic<<endl;
-cout<<"Distancia calculada con metodo adaptativo:   "<<d_adaptive<<endl;
+// imprime solClasica - solAdaptativa 
+cout<<time1<<","<<time2<<","<<solClasica.minDistance(book1,book2)<<","<<solAdaptativa.minDistance(book1,book2)<<endl;
 
 return 0;
 }
